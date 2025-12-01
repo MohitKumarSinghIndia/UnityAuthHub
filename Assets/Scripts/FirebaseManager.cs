cef6a7317ae6c17f501b068f8dac9f45ccc1f3a1
Mohit Kumar Singh <Mohitkumarsingh.india@gmail.com>
2025-12-01 14:35:04 +0530

    Update FirebaseManager.cs

--- Assets/Scripts/FirebaseManager.cs
+++ Assets/Scripts/FirebaseManager.cs
@@ -12,7 +12,7 @@ using UnityEngine.Networking;
 public class FirebaseManager : MonoBehaviour
 {
     [Header("Google API")]
-    private string GoogleAPI = "503252751802-5raqqfln7h2udcjeoohn5jl5dfi0tpur.apps.googleusercontent.com"; // Replace with your actual WebClientID
+    private string GoogleAPI = " "; // Replace with your actual WebClientID
     private GoogleSignInConfiguration configuration;
 
     [Header("Firebase Auth")]
